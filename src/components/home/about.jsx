8ce1d94b58b9dfba16c724799dc32acf1d90f418
krisine <kristine.nyaga@student.moringaschool.com>
2023-09-21 10:26:07 +0300

    making the about section responsive

--- src/components/home/about.jsx
+++ src/components/home/about.jsx
@@ -1,19 +1,19 @@
-import { BsArrowRight } from 'react-icons/bs'
+import { BsArrowRight } from 'react-icons/bs';
+
 const About = () => {
   return (
-<div className='bg-black relative h-[30vh]'>
-  <div className='m-auto max-w-7xl flex items-center justify-center h-full'>
-    <div className='text-center'>
-      <p className='text-white p__smalltext'>who's Kris</p>
-      <p className='text-white text-7xl p__smalltext'>just a girl who loves code</p>
-      <p className="text-white">
-        <span className="p-3 rounded-full border border-white text-white">more about</span> me <BsArrowRight className='inline' />
-      </p>
+    <div className='bg-black lg:h-[40vh] h-[30vh]'>
+      <div className='m-auto flex items-center justify-center h-full'>
+        <div className='text-center flex flex-col gap-4'>
+          <p className='text-white p__smalltext'>who's Kris?</p>
+          <p className='text-white lg:text-7xl text-4xl p__smalltext'>just a girl who loves code</p>
+          <p className="text-white">
+            <span className="p-3 rounded-full border border-white text-white">more about</span> me <BsArrowRight className='inline' />
+          </p>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
-  )
+  );
 }
 
 export default About;
