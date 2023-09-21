9ba62dfc9564deaee556a78fc749a733ede45940
krisine <kristine.nyaga@student.moringaschool.com>
2023-09-21 10:27:27 +0300

    adds the shapes section

--- src/components/home/shapes.jsx
+++ src/components/home/shapes.jsx
@@ -1,8 +1,8 @@
 
 const Shapes = () => {
   return (
-    <div>
-      <div className="flex mt-20 border-b border-lightgray ">
+    <div className="h-[40vh] flex items-center">
+      <div className="flex mt-20 border-b border-lightgray w-full ">
         <div className="w-48 h-48 border rounded-full bg-lightblue border-lightblue" />
         <div className="w-48 h-48 border rounded-full bg-lightblue border-lightblue" />
         <div className="w-48 h-48 border rounded-full bg-lightblue border-lightblue"/>
