3c7750e7e376b04a52d7624761710ea7292981ca
krisine <kristine.nyaga@student.moringaschool.com>
2023-09-21 10:25:50 +0300

    making the process section responsive

--- src/components/home/process.jsx
+++ src/components/home/process.jsx
@@ -1,27 +1,27 @@
 
 const Process = () => {
   return (
-    <div className="m-auto max-w-7xl h-[30vh]">
+    <div className="m-auto max-w-7xl lg:h-[50vh] sm:h-[70vh]">
       <div className="flex justify-center items-center flex-col font_cormorant">
         <h2 className="text-4xl">My Process</h2>
-        <p className="text-xl">Over years of experience I developed a perfect process that invloves all stakeholders.</p>
+        <p className="lg:text-xl text-base text-center">Over years of experience I developed a perfect process that invloves all stakeholders.</p>
 
       </div>
-      <div className="flex justify-between  items-center font_cormorant mt-10">
+      <div className="flex justify-between  items-center font_cormorant mt-10 flex-col lg:flex-row">
         <div className="flex flex-col justify-center items-center text-center font_cormorant">
-          <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-lightblue">01</h3>
-          <h4 className="text-3xl">Research</h4>
-          <p className="text-base max-w-sm font-medium">focussed on understanding your business requirements, users and problems</p>
+          <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-darkblue">01</h3>
+          <h4 className="text-2xl lg:text-3xl font-medium">Research</h4>
+          <p className="text-base max-w-sm font-medium sm:px-3">focussed on understanding your business requirements, users and problems</p>
         </div>
         <div className="flex flex-col justify-center items-center text-center">
-           <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-lightblue">02</h3>
-          <h4 className="text-3xl">Design</h4>
+           <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-darkblue">02</h3>
+          <h4 className="text-2xl lg:text-3xl font-medium">Design</h4>
           <p className="text-base max-w-sm font-medium" >In this stage we work closely to come up with lots of solutions and finalize design.</p>
         </div>
         <div className="flex flex-col justify-center items-center text-center">
-          <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-lightblue">03</h3>
-          <h4 className="text-3xl">Development</h4>
-          <p className="text-base max-w-sm font-medium">I develop your product  and help you with maintainiance</p>
+          <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-darkblue">03</h3>
+          <h4 className="text-2xl lg:text-3xl font-medium">Development</h4>
+          <p className="text-base max-w-sm font-medium sm:px-3">I develop your product  and help you with maintainiance</p>
         </div>
 
       </div>
