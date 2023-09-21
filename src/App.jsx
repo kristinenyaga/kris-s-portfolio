25db027a36e17f56f663940c6bad041792b440de
krisine <kristine.nyaga@student.moringaschool.com>
2023-09-21 10:26:39 +0300

    adds animatepresence for the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
-import Hero from './components/home/hero'
 import Navbar from './components/navbar/navbar'
 import router from './utils/routesconfig';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { AnimatePresence } from 'framer-motion';
 function App() {
   return (
     <Router>
-      <Navbar />
-      {router} {/* Render your routes */}
+      {/* <Navbar /> */}
+      <AnimatePresence mode='wait'>
+      {router} 
+      </AnimatePresence>
     </Router>
   );
 }
