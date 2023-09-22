e52ef7767571ce8805cfff5f7bf7821145817b07
krisine <kristine.nyaga@student.moringaschool.com>
2023-09-22 23:36:02 +0300

    assets to be used

--- src/components/home/process.jsx
+++ src/components/home/process.jsx
@@ -1,17 +1,18 @@
 
 const Process = () => {
   return (
-    <div className="m-auto max-w-7xl lg:h-[50vh] h-[100vh]">
+    <div className="m-auto h-[100vh] w-[90vw]">
       <div className="flex justify-center items-center flex-col font_cormorant">
-        <h2 className="text-4xl">My Process</h2>
-        <p className="lg:text-xl text-base text-center">Over years of experience I developed a perfect process that invloves all stakeholders.</p>
+        <p className=" text-4xl font-noto font-light text-black ">My Process</p>
+        <p className="lg:text-xl text-base text-center">I have developed a perfect process that involves all stakeholders.</p>
 
       </div>
-      <div className="flex justify-between  items-center font_cormorant mt-10 flex-col lg:flex-row">
-        <div className="flex flex-col justify-center items-center text-center font_cormorant">
+      <div className="flex justify-between  items-center font_cormorant mt-4 flex-col lg:flex-row gap-4">
+        <div>
+          <div className="flex flex-col justify-center items-center text-center font_cormorant">
           <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-darkblue">01</h3>
           <h4 className="text-2xl lg:text-3xl font-medium">Research</h4>
-          <p className="text-base max-w-sm font-medium sm:px-3">focussed on understanding your business requirements, users and problems</p>
+          <p className="text-base max-w-sm font-medium sm:px-3 ">Focussed on understanding your business requirements, users and problems.</p>
         </div>
         <div className="flex flex-col justify-center items-center text-center">
            <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-darkblue">02</h3>
@@ -23,6 +24,9 @@ const Process = () => {
           <h4 className="text-2xl lg:text-3xl font-medium">Development</h4>
           <p className="text-base max-w-sm font-medium sm:px-3">I develop your product  and help you with maintainiance</p>
         </div>
+
+        </div>
+        
 
       </div>
     </div>
