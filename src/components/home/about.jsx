1eaa6e1d536ec5688d356773e04e1f9fddae9caf
krisine <kristine.nyaga@student.moringaschool.com>
2023-09-22 23:37:05 +0300

    add hover effect on button

--- src/components/home/about.jsx
+++ src/components/home/about.jsx
@@ -6,9 +6,9 @@ const About = () => {
       <div className='m-auto flex items-center justify-center h-full'>
         <div className='text-center flex flex-col gap-4'>
           <p className='text-white p__smalltext'>who's Kris?</p>
-          <p className='text-white lg:text-7xl text-4xl p__smalltext'>just a girl who loves code</p>
-          <p className="text-white">
-            <span className="p-3 rounded-full border border-white text-white">more about</span> me <BsArrowRight className='inline' />
+          <p className='text-white lg:text-6xl text-3xl p__smalltext'>just a girl who loves code</p>
+          <p className="text-white font_cormorant group cursor-pointer">
+            <span className="p-2 md:p-3 rounded-full border border-white text-white group-hover:bg-white group-hover:text-black transition-all duration-300 ease-in-out">more about</span> me <BsArrowRight className='inline group-hover:scale-100 group-hover:ml-3 ' />
           </p>
         </div>
       </div>
