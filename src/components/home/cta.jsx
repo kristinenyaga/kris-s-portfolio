36259d8f7f257bd8640f653a84e5c7655cc28685
krisine <kristine.nyaga@student.moringaschool.com>
2023-09-22 23:37:31 +0300

    adds hover on button

--- src/components/home/cta.jsx
+++ src/components/home/cta.jsx
@@ -2,10 +2,10 @@ import { BsArrowUpRight } from 'react-icons/bs'
 
 const Cta = () => {
   return (
-    <div className='bg-black'>
+    <div className=''>
       <div className='m-auto max-w-7xl h-[50vh] flex justify-center items-center flex-col'>
-        <p className='text-white font_cormorant text-4xl lg:text-7xl max-w-3xl text-center'>Let's transform your vision to design & code.</p>
-        <button className='bg-white flex justify-center items-center font_cormorant text-xl px-5 gap-2 py-2 rounded-md hover:bg-black hover:border hover:border-white'>get in touch  <BsArrowUpRight /></button>
+        <p className='text-black font_cormorant text-3xl sm:text-4xl md:text-5xl lg:text-6xl max-w-3xl text-center'>Let's transform your vision to design & code.</p>
+        <button className='bg-black text-white flex justify-center items-center font_cormorant text-base px-5 gap-2 py-2 rounded-md hover:bg-white hover:border hover:border-black hover:text-black'>get in touch  <BsArrowUpRight /></button>
       </div>
     </div>
   )
