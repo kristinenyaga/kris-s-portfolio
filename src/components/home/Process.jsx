e7b74a22ba8adf0386340e90c3cd69c63737e065
krisine <kristine.nyaga@student.moringaschool.com>
2023-09-25 22:58:12 +0300

    homepage:myprocess section

---
+++ src/components/home/Process.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import designthinking from '../../assets/design-thinking.png'
+import layers from '../../assets/layers.png'
+import researchanddevelopment from '../../assets/research-and-development.png'
+const Process = () => {
+  return (
+    <div className='h-[100vh] flex flex-col max-w-7xl  m-auto'>
+      <div className='flex flex-col justify-center items-center'>
+        <p className='font_poppins text-base font-thin'>My Process</p>
+        <p className='font_opensans text-4xl max-w-2xl text-center '>I have developed  process that involves all stakeholders.</p>
+
+      </div>
+      <div className='flex mt-32 justify-between'>
+        <div>
+          <img src={researchanddevelopment} />
+          <div className='border-y border-lightgray py-6 mt-6 '>
+            <p className='font_albertsans text-4xl'>Research</p>
+            <p className="text-base  font-medium max-w-sm">Focussed on understanding your business requirements, users and problems.</p>
+          </div>
+        </div>
+        <div>
+          <img src={designthinking} />
+          <div className='border-y border-lightgray py-6 mt-6 '>
+            <p className='font_albertsans text-4xl'>Design</p>
+            <p className="text-base  font-medium max-w-sm">In this stage we work closely to come up with lots of solutions and finalize design.</p>
+          </div>
+        </div>
+        <div className=''>
+          <img src={layers} className='' />
+          <div className='border-y border-lightgray py-6 mt-6 '>
+            <p className='font_albertsans text-4xl'>Development</p>
+            <p className="text-base  font-medium max-w-sm">I develop your product  and help you with maintainiance</p>
+          </div>
+        </div>     
+      </div>
+    </div>
+  )
+}
+
+export default Process
--- src/components/home/process.jsx
+++ src/components/home/process.jsx
@@ -1,34 +1,35 @@
 
 const Process = () => {
   return (
-    <div className="m-auto h-[100vh] w-[90vw]">
-      <div className="flex justify-center items-center flex-col font_cormorant">
-        <p className=" text-4xl font-noto font-light text-black ">My Process</p>
-        <p className="lg:text-xl text-base text-center">I have developed a perfect process that involves all stakeholders.</p>
-
+    <div className="m-auto h-[100vh] bg-lightgray flex justify-center items-center text-black">
+      <div className="w-[80vw] bg-white flex justify-between">
+        <div className="flex flex-col font_poppins text-darkblue max-w-md">
+          <p className=" text-4xl font-noto font-light">My Process</p>
+          <p className="text-sm ">I have developed a perfect process that involves all stakeholders.</p>
+        </div>
+        <div className="flex justify-between  items-center font_opensans mt-4  gap-4 m-auto">
+          <div>
+          </div>
+          <div className="flex">
+            <div className="flex flex-col justify-center items-center text-center border p-3">
+              <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-xl">01</h3>
+              <h4 className="text-2xl font-medium">Research</h4>
+              <p className="text-base max-w-sm font-medium sm:px-3 ">Focussed on understanding your business requirements, users and problems.</p>
+            </div>
+            <div className="flex flex-col justify-center items-center text-center border p-3">
+              <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl">02</h3>
+              <h4 className="text-2xl font-medium">Design</h4>
+              <p className="text-base max-w-sm font-medium" >In this stage we work closely to come up with lots of solutions and finalize design.</p>
+            </div>
+            <div className="flex flex-col justify-center items-center text-center border p-3">
+              <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl">03</h3>
+              <h4 className="text-2xl font-medium">Development</h4>
+              <p className="text-base max-w-sm font-medium sm:px-3">I develop your product  and help you with maintainiance</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex justify-between  items-center font_cormorant mt-4 flex-col lg:flex-row gap-4">
-        <div>
-          <div className="flex flex-col justify-center items-center text-center font_cormorant">
-          <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-darkblue">01</h3>
-          <h4 className="text-2xl lg:text-3xl font-medium">Research</h4>
-          <p className="text-base max-w-sm font-medium sm:px-3 ">Focussed on understanding your business requirements, users and problems.</p>
-        </div>
-        <div className="flex flex-col justify-center items-center text-center">
-           <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-darkblue">02</h3>
-          <h4 className="text-2xl lg:text-3xl font-medium">Design</h4>
-          <p className="text-base max-w-sm font-medium" >In this stage we work closely to come up with lots of solutions and finalize design.</p>
-        </div>
-        <div className="flex flex-col justify-center items-center text-center">
-          <h3 className="border w-14 h-14 justify-center items-center flex rounded-full text-3xl text-darkblue">03</h3>
-          <h4 className="text-2xl lg:text-3xl font-medium">Development</h4>
-          <p className="text-base max-w-sm font-medium sm:px-3">I develop your product  and help you with maintainiance</p>
-        </div>
-
-        </div>
         
-
-      </div>
     </div>
   )
 }
