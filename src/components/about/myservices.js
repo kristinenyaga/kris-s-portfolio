d463d0cbb168eadad9e011c4bc79eba677558ed2
krisine <kristine.nyaga@student.moringaschool.com>
2024-02-04 10:03:56 +0300

    adds an array of myservices list

---
+++ src/components/about/myservices.js
@@ -0,0 +1,30 @@
+export const servicesList = [
+  {
+    name: "Frontend Development",
+    description:
+      "Rhoncus mauris vel egestas elit enas hend rerit amet, ut commodo aiverra ut pellen tesque commodo orci.",
+    image:
+      "https://assets-global.website-files.com/651aa8b4feb6490e2b81db9a/651ab09b11b4ed7aadd49365_services-decor.svg",
+  },
+  {
+    name: "Frontend Development",
+    description:
+      "Rhoncus mauris vel egestas elit enas hend rerit amet, ut commodo aiverra ut pellen tesque commodo orci.",
+    image:
+      "https://assets-global.website-files.com/651aa8b4feb6490e2b81db9a/651ab09b11b4ed7aadd49365_services-decor.svg",
+  },
+  {
+    name: "UI/UX design",
+    description:
+      "Rhoncus mauris vel egestas elit enas hend rerit amet, ut commodo aiverra ut pellen tesque commodo orci.",
+    image:
+      "https://assets-global.website-files.com/651aa8b4feb6490e2b81db9a/651ab09b11b4ed7aadd49365_services-decor.svg",
+  },
+  {
+    name: "Frontend Development",
+    description:
+      "Rhoncus mauris vel egestas elit enas hend rerit amet, ut commodo aiverra ut pellen tesque commodo orci.",
+    image:
+      "https://assets-global.website-files.com/651aa8b4feb6490e2b81db9a/651ab09b11b4ed7aadd49365_services-decor.svg",
+  }
+];
