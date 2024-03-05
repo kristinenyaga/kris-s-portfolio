9240140dbf5e482a6dc0e730298d397a8874b502
krisine <kristine.nyaga@student.moringaschool.com>
2024-03-05 04:16:05 +0300

    adding utils

---
+++ src/utils/loadingScreen.jsx
@@ -0,0 +1,12 @@
+import {InfinitySpin} from 'react-loader-spinner'
+
+function LoadingScreen() {
+  return (
+    <div className="loading-screen  bg-black text-white">
+      <InfinitySpin color="grey" />
+      <p className='font_opensans text-5xl opacity-70'>kris's portfolio...</p>
+    </div>
+  );
+}
+
+export default LoadingScreen;
