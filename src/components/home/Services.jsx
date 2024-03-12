53f2dc2fe94cc5f659cdc3d442a77db265fbc067
krisine <kristine.nyaga@student.moringaschool.com>
2024-03-12 16:58:44 +0300

    changes the text on services section

--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -19,20 +19,20 @@ const Services = () => {
             <div className='flex flex-col gap-4'>
               <p className='md:text-3xl text-2xl text-darkblue '>UI/UX design</p>
                 <p className=' leading-7 text-sm lg:text-base
-                  tracking-wide  max-w-xl'>Ready to embark on a design adventure with me? <br /> Let's work together to create user-friendly and captivating UI/UX designs for your project. Whether it's a web app, mobile app, I'm here to bring creativity to your project.</p>
+                  tracking-wide  max-w-xl'>Ready to embark on a design adventure with me? <br /> Let's work together to create user-friendly and captivating UI/UX designs for your project. Whether it's a web app, mobile app, I'm here to bring your project to life.</p>
             </div>
-           <button className='border border-black text-black flex justify-center items-center text-[14px] px-5 gap-2 py-2 mt-4 rounded-md hover:bg-black hover:border hover:border-white hover:text-white' onClick={navigate}>get in touch  <BsArrowUpRight /></button>
+           <button className='border border-black text-black flex justify-center items-center text-[14px] px-5 gap-2 md:py-2 py-1.5 mt-4 rounded-md hover:bg-black hover:border hover:border-white hover:text-white' onClick={navigate}>get in touch  <BsArrowUpRight /></button>
         </div>
           <img src={cornerstonepatternsright} className=' h-36 hidden md:block' />
         </div>
-        <div className='flex  gap-10 flex-row-reverse items-center justify-between lg:mt-20 mt-5'>
+        <div className='flex lg:gap-10 flex-row-reverse items-center justify-between lg:mt-20 mt-5'>
           <div>
             <div className='flex flex-col gap-4'>
-              <p className='md:text-3xl text-2xl text-darkblue '>web/mobile Development</p>
+              <p className='md:text-3xl text-2xl text-darkblue '>web Development</p>
                 <p className='text-sm lg:text-base
                   tracking-wide leading-7 max-w-xl'>Ready to embark on a design adventure with me? <br /> Let's work together to create user-friendly and captivating UI/UX designs for your project. Whether it's a web app, mobile app, I'm here to bring creativity to your project.</p>
             </div>
-           <button className='border border-black text-black flex justify-center items-center text-[14px] px-5 gap-2 py-2 mt-4 rounded-md hover:bg-black hover:border hover:border-white hover:text-white' onClick={navigate}>get in touch  <BsArrowUpRight /></button>
+           <button className='border border-black text-black flex justify-center items-center text-[14px] px-5 gap-2 md:py-2 py-1.5 mt-4 rounded-md hover:bg-black hover:border hover:border-white hover:text-white' onClick={navigate}>get in touch  <BsArrowUpRight /></button>
         </div>
           <img src={cornerstonepatternsright} className=' h-36 hidden md:block opacity-90' />
       </div>
