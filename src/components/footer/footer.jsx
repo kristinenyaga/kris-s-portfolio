16bbbd88a60664dba15223b7c6eff7cc85e61393
krisine <kristine.nyaga@student.moringaschool.com>
2025-08-04 23:07:11 +0300

    update on footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className='max-w-full bg-black h-[60vh] relative section'>
+    <div className='max-w-full bg-black border-t border-lightgray h-[60vh] relative section'>
       <div className='flex flex-col justify-between items-center gap-14'>
         <p className='absolute left-20 md:left-14 top-7 md:top-20 circle font_poppins text-lightgray'>hello</p>
         <div className='flex justify-items-center gap-3 flex-col content-center items-center text-white font_poppins md:pt-28 pt-20'>
